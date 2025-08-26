864f42bc256273eb29dfc05747c61240c588100d
Devashish <workfordevashishhaldar@gmail.com>
2025-08-26 06:16:38 +0000

    Code challenge submission for question: 164 Maximum Gap

---
+++ 164 Maximum Gap/164maximum-gap.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        int n=nums.size();
+        if(n<2) return 0;
+        sort(nums.begin(),nums.end());
+        int dist=INT_MIN;
+        for(int i=1;i<n;i++){
+            int diff=nums[i]-nums[i-1];
+            if(dist<diff){
+                dist=diff;
+            }
+        }
+        return dist;
+    }
+};

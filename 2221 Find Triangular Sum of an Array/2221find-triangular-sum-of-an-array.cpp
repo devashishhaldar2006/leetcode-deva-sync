d9ace7731b38280a652056fa45c85718420fce3b
Devashish <workfordevashishhaldar@gmail.com>
2025-09-30 18:40:19 +0000

    Code challenge submission for question: 2221 Find Triangular Sum of an Array

---
+++ 2221 Find Triangular Sum of an Array/2221find-triangular-sum-of-an-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int triangularSum(vector<int>& nums) {
+        for(int sz= nums.size();sz> 0;sz--) {
+            for (int i=0; i<sz-1;i++) {
+                nums[i]=(nums[i]+nums[i+1])%10;
+            }
+        }
+        return nums[0];
+    }
+};

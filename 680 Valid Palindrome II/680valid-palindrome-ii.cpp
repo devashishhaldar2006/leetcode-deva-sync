a4b1c90cc8c13c4697d339d87a2e9553de2b4ce2
Devashish <workfordevashishhaldar@gmail.com>
2025-10-13 06:36:58 +0000

    Code challenge submission for question: 680 Valid Palindrome II

---
+++ 680 Valid Palindrome II/680valid-palindrome-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(const string& s,int left,int right) {
+        while(left<right) {
+            if(s[left]!=s[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int i=0,j=s.size()-1;
+        while(i<j) {
+            if(s[i]!=s[j]) {
+                return isPalindrome(s,i+1,j) || isPalindrome(s,i,j-1);
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};

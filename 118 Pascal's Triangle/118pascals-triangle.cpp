19611adff679140fe20ff1202049904cd43fd626
Devashish <workfordevashishhaldar@gmail.com>
2026-05-28 16:19:10 +0000

    Code challenge submission for question: 118 Pascal's Triangle

---
+++ 118 Pascal's Triangle/118pascals-triangle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+        for(int i=0;i<numRows;i++){
+            vector<int> v(i+1);
+            ans.push_back(v);
+        }
+        for(int i=0;i<numRows;i++){
+            for(int j=0;j<=i;j++){
+                if(j==0 || j==i){
+                    ans[i][j]=1;
+                }
+                else ans[i][j]=ans[i-1][j-1]+ans[i-1][j];
+            }
+        }
+        return ans;
+    }
+};

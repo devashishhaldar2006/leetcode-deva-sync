0b3eb6fc1d291db7040b4ca31174a084224a43d9
Devashish <workfordevashishhaldar@gmail.com>
2026-05-29 08:50:35 +0000

    Code challenge submission for question: 205 Isomorphic Strings

---
+++ 205 Isomorphic Strings/205isomorphic-strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        vector<int> v(150,1000);
+        for(int i=0;i<s.size();i++){
+            int idx=(int)s[i];
+            if(v[idx]==1000) v[idx]=s[i]-t[i];
+            else if(v[idx]!=(s[i]-t[i])) return false;
+        }
+        for(int i=0;i<150;i++){
+            v[i]=1000;
+        }
+        for(int i=0;i<s.size();i++){
+            int idx=(int)t[i];
+            if(v[idx]==1000) v[idx]=s[i]-t[i];
+            else if(v[idx]!=(s[i]-t[i])) return false;
+        }
+        return true;
+    }
+};

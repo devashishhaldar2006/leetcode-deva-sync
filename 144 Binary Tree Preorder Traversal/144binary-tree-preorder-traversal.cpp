c91e7e35aaac412d8bd09cd5f071420858938a33
Devashish <workfordevashishhaldar@gmail.com>
2026-06-13 19:52:27 +0000

    Code challenge submission for question: 144 Binary Tree Preorder Traversal

---
+++ 144 Binary Tree Preorder Traversal/144binary-tree-preorder-traversal.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        if(!root) return {};
+        vector<int> ans;
+        stack<TreeNode*> st;
+        st.push(root);
+        while(st.size()){
+            TreeNode* temp=st.top();
+            st.pop();
+            ans.push_back(temp->val);
+            if(temp->right) st.push(temp->right);
+            if(temp->left) st.push(temp->left);
+        }
+        return ans;
+    }
+};

043a19c0f9b549ee196f8cd3369cee820130c65f
Devashish <workfordevashishhaldar@gmail.com>
2026-06-15 20:01:06 +0000

    Code challenge submission for question: 1038 Binary Search Tree to Greater Sum Tree

---
+++ 1038 Binary Search Tree to Greater Sum Tree/1038binary-search-tree-to-greater-sum-tree.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    TreeNode* helper(TreeNode* root,int& sum){
+        if(!root) return NULL;
+        helper(root->right,sum);
+        sum+=root->val;
+        root->val=sum;
+        helper(root->left,sum);
+        return root;
+    }
+    TreeNode* bstToGst(TreeNode* root) {
+        if(!root) return root;
+        int sum=0;
+        return helper(root,sum);
+    }
+};

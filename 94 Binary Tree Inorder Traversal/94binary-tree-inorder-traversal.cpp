28e83553bf31504a90c09f453f5a86aae8eee3c7
Devashish <workfordevashishhaldar@gmail.com>
2026-06-16 14:37:04 +0000

    Code challenge submission for question: 94 Binary Tree Inorder Traversal

---
+++ 94 Binary Tree Inorder Traversal/94binary-tree-inorder-traversal.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        TreeNode* curr=root;
+        vector<int> ans;
+        while(curr){
+            if(curr->left){
+                // find pred
+                TreeNode* pred=curr->left;
+                while(pred->right && pred->right!=curr){
+                    pred=pred->right;
+                }
+                if(!pred->right){
+                    pred->right=curr;
+                    curr=curr->left;
+                }
+                if(pred->right==curr){
+                    pred->right=NULL;
+                    ans.push_back(curr->val);
+                    curr=curr->right;
+                }
+                
+            }
+            else{
+                ans.push_back(curr->val);
+                curr=curr->right;
+            }
+        }
+        return ans;
+    }
+};

8a594339dff50376c5295c42a4dafb59c81560cc
Devashish <workfordevashishhaldar@gmail.com>
2026-06-30 13:59:33 +0000

    Code challenge submission for question: 63 Unique Paths II

---
+++ 63 Unique Paths II/63unique-paths-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int helper(vector<vector<int>>& arr,int m,int n,vector<vector<int>>& dp){
+        if(m==0 && n==0 and arr[m][n]==1) return 0;
+        if(m<0 || n<0) return 0;
+        if(arr[m][n]==1) dp[m][n]=0;
+        if(m==0 && n==0) return 1;
+        if(dp[m][n]!=-1) return dp[m][n];
+        return dp[m][n]=helper(arr,m-1,n,dp)+helper(arr,m,n-1,dp);
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& arr) {
+        int m=arr.size();
+        int n=arr[0].size();
+        vector<vector<int>> dp(m+1,vector<int> (n+1,-1));
+        return helper(arr,m-1,n-1,dp);
+    }
+};

651889f60976df916174905178506930c4f1d32a
Devashish <workfordevashishhaldar@gmail.com>
2026-07-06 17:28:42 +0000

    Code challenge submission for question: 300 Longest Increasing Subsequence

---
+++ 300 Longest Increasing Subsequence/300longest-increasing-subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n,0);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    dp[i]=max(dp[i],dp[j]);
+                }
+            }
+            dp[i]++;
+        }
+        return *max_element(dp.begin(),dp.end());
+    }
+};

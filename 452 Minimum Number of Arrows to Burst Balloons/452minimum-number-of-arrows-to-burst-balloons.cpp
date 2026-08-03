369fbbc269b7a2194145c3b7c4183d37bf9021ff
Devashish <workfordevashishhaldar@gmail.com>
2026-08-03 18:07:42 +0000

    Code challenge submission for question: 452 Minimum Number of Arrows to Burst Balloons

---
+++ 452 Minimum Number of Arrows to Burst Balloons/452minimum-number-of-arrows-to-burst-balloons.cpp
@@ -0,0 +1,20 @@
+bool cmp(vector<int>& a,vector<int>& b){
+    return a[0]<b[0];
+}
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end(),cmp);
+        int n=intervals.size();
+        int ans=0;
+        int lastInterval=intervals[0][1];
+        for(int i=1;i<n;i++){
+            if(intervals[i][0]<=lastInterval){
+                ans++;
+                lastInterval=min(lastInterval,intervals[i][1]);
+            }
+            else lastInterval=intervals[i][1];
+        }
+        return intervals.size()-ans;
+    }
+};

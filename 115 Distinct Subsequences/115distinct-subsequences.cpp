15cef0402fa7d00eb2084748d7d1b7832742ed9c
Devashish <workfordevashishhaldar@gmail.com>
2026-09-06 08:32:33 +0000

    Code challenge submission for question: 115 Distinct Subsequences

---
+++ 115 Distinct Subsequences/115distinct-subsequences.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int helper(string s,string t,int i,int j,vector<vector<int>>& dp){
+        if(j==0) return 1;
+        if(i==0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s[i-1]==t[j-1]){
+            return dp[i][j]=helper(s,t,i-1,j,dp)+helper(s,t,i-1,j-1,dp);
+        }
+        else {
+            return dp[i][j]=helper(s,t,i-1,j,dp);
+        }
+    }
+    int numDistinct(string s, string t) {
+        int m=s.size();
+        int n=t.size();
+        vector<vector<int>> dp(m+1,vector<int> (n+1,-1));
+        return helper(s,t,m,n,dp);
+    }
+};

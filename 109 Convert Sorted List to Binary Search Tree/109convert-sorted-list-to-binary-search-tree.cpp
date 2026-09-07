b6a0e30eda487c74530d9576c163bd14cacee59d
Devashish <workfordevashishhaldar@gmail.com>
2026-09-07 10:49:45 +0000

    Code challenge submission for question: 109 Convert Sorted List to Binary Search Tree

---
+++ 109 Convert Sorted List to Binary Search Tree/109convert-sorted-list-to-binary-search-tree.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    TreeNode* helper(int lo,int hi,vector<int>& nums){
+        if(lo>hi) return NULL;
+        int mid=lo+(hi-lo)/2;
+        TreeNode* root=new TreeNode(nums[mid]);
+        root->left=helper(lo,mid-1,nums);
+        root->right=helper(mid+1,hi,nums);
+        return root;
+    }
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        int n=nums.size();
+        return helper(0,n-1,nums);
+    }
+    TreeNode* sortedListToBST(ListNode* head) {
+        vector<int> arr;
+        ListNode* temp=head;
+        while(temp){
+            arr.push_back(temp->val);
+            temp=temp->next;
+        }
+        return sortedArrayToBST(arr);
+    }
+};

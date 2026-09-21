c7301022165db4ac060c3b0632a6a3b11f15197a
Devashish Haldar <workfordevashishhaldar@gmail.com>
2026-09-21 16:15:24 +0530

    Time: 26 ms (62.21%) | Memory: 17.4 MB (46.33%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,38 +1,30 @@
 class Solution {
 public:
-    void bfs(int i,int j,vector<vector<char>>& grid,vector<vector<int>>& visit){
+    void dfs(int row,int col,vector<vector<char>>& grid,vector<vector<int>>& visit){
         int m=grid.size();
         int n=grid[0].size();
-        visit[i][j]=1;
-        queue<pair<int,int>> q;
-        q.push({i,j});
-        while(q.size()){
-            int row=q.front().first;
-            int col=q.front().second;
-            q.pop();
-            if(row>0){
-                if(grid[row-1][col]=='1' and visit[row-1][col]==0){
-                    visit[row-1][col]=1;
-                    q.push({row-1,col});
-                }
+        if(row>0){
+            if(grid[row-1][col]=='1' and visit[row-1][col]==0){
+                visit[row-1][col]=1;
+                dfs(row-1,col,grid,visit);
             }
-            if(row+1<m){
-                if(grid[row+1][col]=='1' and visit[row+1][col]==0){
-                    visit[row+1][col]=1;
-                    q.push({row+1,col});
-                }
+        }
+        if(row+1<m){
+            if(grid[row+1][col]=='1' and visit[row+1][col]==0){
+                visit[row+1][col]=1;
+                dfs(row+1,col,grid,visit);
             }
-            if(col>0){
-                if(grid[row][col-1]=='1' and visit[row][col-1]==0){
-                    visit[row][col-1]=1;
-                    q.push({row,col-1});
-                }
+        }
+        if(col>0){
+            if(grid[row][col-1]=='1' and visit[row][col-1]==0){
+                visit[row][col-1]=1;
+                dfs(row,col-1,grid,visit);
             }
-            if(col+1<n){
-                if(grid[row][col+1]=='1' and visit[row][col+1]==0){
-                    visit[row][col+1]=1;
-                    q.push({row,col+1});
-                }
+        }
+        if(col+1<n){
+            if(grid[row][col+1]=='1' and visit[row][col+1]==0){
+                visit[row][col+1]=1;
+                dfs(row,col+1,grid,visit);
             }
         }
     }
@@ -43,12 +35,13 @@ public:
         int cnt=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]=='1' && visit[i][j]==0){
-                    bfs(i,j,grid,visit);
+                if(grid[i][j]=='1' and visit[i][j]==0){
+                    dfs(i,j,grid,visit);
                     cnt++;
                 }
             }
         }
         return cnt;
+
     }
 };

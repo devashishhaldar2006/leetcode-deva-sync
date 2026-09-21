34db5947b8927b2cf796e0be9d1ba69e8b3d65b0
Devashish Haldar <workfordevashishhaldar@gmail.com>
2026-09-21 16:16:10 +0530

    Time: 30 ms (32.80%) | Memory: 17.5 MB (46.33%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -36,6 +36,7 @@ public:
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]=='1' and visit[i][j]==0){
+                    visit[i][j]=1;
                     dfs(i,j,grid,visit);
                     cnt++;
                 }

9cb18d50bac5a4fccfece8655a6c002d42af9124
Devashish Haldar <workfordevashishhaldar@gmail.com>
2026-09-24 15:07:46 +0530

    Time: 7 ms (0.47%) | Memory: 21.4 MB (95.21%) - LeetSync

---
+++ 669-trim-a-binary-search-tree/trim-a-binary-search-tree.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void helper(TreeNode* root,int lo,int hi){
+        if(!root) return;
+        helper(root->left,lo,hi);
+        helper(root->right,lo,hi);
+        while(root->left){
+            if(root->left->val<lo){
+                root->left=root->left->right;
+            }
+            else if(root->left->val>hi){
+                root->left=root->left->left;
+            }
+            else break;
+        }
+        while(root->right){
+            if(root->right->val>hi){
+                root->right=root->right->left;
+            }
+            else if(root->right->val<lo){
+                root->right=root->right->right;
+            }
+            else break;
+        }
+        helper(root->left,lo,hi);
+        helper(root->right,lo,hi);
+    }
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        TreeNode* dummy=new TreeNode(INT_MAX);
+        dummy->left=root;
+        helper(dummy,low,high);
+        return dummy->left;
+    }
+};
